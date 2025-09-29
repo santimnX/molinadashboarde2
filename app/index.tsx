b361b979fffc1530c613be184d95593e58c6550e
ssanti04072008 <ssanti04072008@gmail.com>
2025-09-29 12:37:28 +0000

    💄ui(Profiledetails):Crear los detalles principales del usuario     -Añadir imagen de perfil     -Añadir nombre y profesion del usuario

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,10 @@
-import { Text, View } from "react-native";
+import ProfileDetails from "@/components/ProfileDetails";
+import { View } from "react-native";
 import "../global.css";
 export default function Index() {
   return (
-    <View className="flex justify-center items-center bg-white-screen h-screen">
-        
+    <View className="flex justify-center items-center bg-white w-screen h-screen">
+      <ProfileDetails></ProfileDetails>
     </View>
   );
 }
