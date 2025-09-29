3f9804f0fdbd9cf8635083ece1678b0e7ae1d8d1
ssanti04072008 <ssanti04072008@gmail.com>
2025-09-29 12:45:00 +0000

    ✨feat(IconButton): Crear un IconButton Component con props     - Uso de pressable y expo-icons para crear un boton.     - Añadir props color:string, iconName:string, pulsar:()=>void, enlace:string     - (ProfileDetails) añadir 4 IconButton

--- components/ProfileDetails.tsx
+++ components/ProfileDetails.tsx
@@ -1,12 +1,19 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Image, Text } from 'react-native';
+import React from 'react';
 
 const Profiledetails = () => {
   return (
-    <View>
-      <Text>profile-details</Text>
+    // Este View ahora solo agrupa el contenido del perfil
+    // y centra sus propios elementos horizontalmente.
+    <View className='items-center'>
+      <Image 
+        source={require('../../assets/images/icon.png')} 
+        className="h-36 w-36 rounded-full mb-4"
+      />
+      <Text className='text-2xl font-bold'>Santiago Molina 3E2</Text>
+      <Text className='font-bold text-gray-400'>Student</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Profiledetails
+export default Profiledetails;
